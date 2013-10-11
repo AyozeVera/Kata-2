785155940adf010167bf8742a7d75e8243fbea5d
usuario <usuario@10.22.146.130>
2013-10-11 12:18:27 +0100

    Cuarto commit método nuevo para devolver el vector con el numero mas frecuente y su frecuencia.

--- src/kata2/MostFrecuentlyUsed.java
+++ src/kata2/MostFrecuentlyUsed.java
@@ -4,22 +4,32 @@ import java.util.HashMap;
 
 public class MostFrecuentlyUsed {
     private final int[] numbersArray;
-    private HashMap <Integer, Integer> frecuencyMap;
+    private HashMap <Integer, Integer> frequencyMap;
+    private int[] mostFrequent = {-1,0};
     
     public MostFrecuentlyUsed (int[] numbersArray){
         this.numbersArray = numbersArray;
     }
     
-    public HashMap<Integer, Integer> FrequencyHashMap (int[] numbersArray){
-        frecuencyMap = new HashMap<>();
+    public void frequencyHashMap (){
+        frequencyMap = new HashMap<>();
         for (int value : numbersArray) {
-            frecuencyMap.put(value, getFrequency(value)+1);
+            frequencyMap.put(value, getFrequency(value)+1);
         }
-        return frecuencyMap;
     }
 
     private int getFrequency(int value) {
-        if (frecuencyMap.containsKey(value))return frecuencyMap.get(value);
+        if (frequencyMap.containsKey(value))return frequencyMap.get(value);
         return 0;
+    }
+    
+    private int[] MostFrecuentlyNumber (HashMap <Integer,Integer> frequencyHashMap){
+        frequencyHashMap();
+        int maxFrequency = 0;
+        int mostFrecuentNumber = -1;
+        for (Integer Value : frequencyMap.keySet()) {
+            if ()
+        }
+        return mostFrequent;
     }
 }
