f95f8d06a29a2000b05bc57372fa3a7897549344
usuario <usuario@10.22.146.130>
2013-10-11 11:39:21 +0100

    Tercer commit método nuevo para calcular la frecuencia de aparición de un valor.

--- src/kata2/MostFrecuentlyUsed.java
+++ src/kata2/MostFrecuentlyUsed.java
@@ -1,16 +1,25 @@
-
 package kata2;
-import java.util.*;
+
+import java.util.HashMap;
 
 public class MostFrecuentlyUsed {
     private final int[] numbersArray;
+    private HashMap <Integer, Integer> frecuencyMap;
     
     public MostFrecuentlyUsed (int[] numbersArray){
         this.numbersArray = numbersArray;
     }
     
-    public Hashmap arrayToHashMap (int[] numbersArray){
-        Hashmap <Integer, Integer> numbersHashMap = new Hashmap <>;
-        return numbersHashMap;
+    public HashMap<Integer, Integer> FrequencyHashMap (int[] numbersArray){
+        frecuencyMap = new HashMap<>();
+        for (int value : numbersArray) {
+            frecuencyMap.put(value, getFrequency(value)+1);
+        }
+        return frecuencyMap;
+    }
+
+    private int getFrequency(int value) {
+        if (frecuencyMap.containsKey(value))return frecuencyMap.get(value);
+        return 0;
     }
 }
