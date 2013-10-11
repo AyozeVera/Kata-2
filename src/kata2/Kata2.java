1f53a7e85a551205c9585591bdabde34437fcb2b
usuario <usuario@10.22.146.130>
2013-10-11 12:25:24 +0100

    Quinto commit faltan las pruebas.

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,8 +1,10 @@
 
 package kata2;
 
-public class Kata2 {
+public class Kata2{
+    private static int[] v = {1,5,3,2,1,3,54,5,4,3,5};
     public static void main(String[] args) {
-       
+       MostFrecuentlyUsed Prueba1 = new MostFrecuentlyUsed (v);
+       System.out.println(MostFrecuentlyNumber(Prueba1));
     }
 }
