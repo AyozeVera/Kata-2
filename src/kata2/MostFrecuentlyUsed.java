56f955437515364b3000f16b812af9d2f475ceb5
usuario <usuario@10.22.146.130>
2013-10-11 11:19:13 +0100

    Segundo commit creando una clase para calcular el array.

---
+++ src/kata2/MostFrecuentlyUsed.java
@@ -0,0 +1,16 @@
+
+package kata2;
+import java.util.*;
+
+public class MostFrecuentlyUsed {
+    private final int[] numbersArray;
+    
+    public MostFrecuentlyUsed (int[] numbersArray){
+        this.numbersArray = numbersArray;
+    }
+    
+    public Hashmap arrayToHashMap (int[] numbersArray){
+        Hashmap <Integer, Integer> numbersHashMap = new Hashmap <>;
+        return numbersHashMap;
+    }
+}
